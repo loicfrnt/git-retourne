8e06dacacf5aead045efcc1531d166ab80f23c3c
Loïc Fournerat <lfournerat@corp.free.fr>
2024-08-19 11:54:45 +0200

    feat: can now use main OR master, and refactor branchExists function for readability

--- git-retourne.sh
+++ git-retourne.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
+branchExists(){
+    if git show-ref --verify --quiet refs/heads/$1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+# argument 1: target branch to delete, fetch and return to
 branch=$1
 
-# Check if a branch is provided
+# if there is no branch called master, use main instead
+if branchExists master; then 
+    mainBranch='master'
+else
+    mainBranch='main'
+
+    if branchExists main; then :;
+    else 
+        echo "No branch called main or master exists."
+        exit 1
+    fi
+fi
+
+
+# Check if a target branch is provided in arguments, otherwise use the current branch
 if [ -z "$branch" ]; then
     branch=$(git branch --show-current)
 fi
 
-# Check if the branch is master
-if [ "$branch" = "master" ]; then
-    echo "You cannot delete the master branch."
+# Check if the branch is mainBranch
+if [ "$branch" = $mainBranch ]; then
+    echo "You cannot delete the $mainBranch branch."
     exit 1
 fi
 
 # Check if the branch exists
-git show-ref --verify --quiet refs/heads/${branch}
-if [ $? -ne 0 ]; then
+if branchExists $branch; then :;
+else
     echo "Branch '$branch' does not exist."
     exit 1
 fi
@@ -26,8 +50,8 @@ read -p "Are you sure you want to delete branch '$branch'? (Y/N): " answer
 # Check user's answer
 case ${answer:0:1} in
     y|Y )
-        # Checkout master and delete the branch
-        git checkout master && git branch -D "$branch" && git fetch -p && git checkout "$branch"
+        # Checkout mainBranch and delete the branch
+        git checkout "$mainBranch" && git branch -D "$branch" && git fetch -p && git checkout "$branch"
         ;;
     * )
         echo "Branch deletion canceled."
